a181d99ac811f0efe9143efa3599d24df6cb2697
CodingWilsonA <wilson.audoin@gmail.com>
2022-04-06 19:48:56 +0200

    Added likes/dislikes feature

--- routes/sauce-router.js
+++ routes/sauce-router.js
@@ -4,11 +4,12 @@ const router = express.Router()
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
 
-const {getAllSauces, getOneSauce, createSauce, modifySauce, deleteSauce} = require('../controllers/sauce-controller')
+const {getAllSauces, getOneSauce, createSauce, modifySauce, deleteSauce, likeDislike} = require('../controllers/sauce-controller')
 
 router.get('/', auth, getAllSauces)
 router.get('/:id', auth, getOneSauce)
 router.post('/', auth, multer, createSauce)
+router.post('/:id/like', auth, likeDislike)
 router.put('/:id', auth, multer, modifySauce)
 router.delete('/:id', auth, deleteSauce)
 
