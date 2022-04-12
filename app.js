a77bfb3bce89c36318b5ca15f7607af3c057e806
CodingWilsonA <wilson.audoin@gmail.com>
2022-04-12 20:23:39 +0200

    Fixed rate limit scope

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(helmet())
-app.use('/api', apiLimiter)
+app.use('/api/auth/login', apiLimiter)
 app.use('/api/auth', userRoutes)
 app.use('/api/sauces', sauceRoutes)
 
